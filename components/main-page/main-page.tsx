f03f91cc82b0baec37b96dbc42dddcec07f7d9d3
RidloGhifary <ridloghfry@gmail.com>
2024-08-01 14:05:46 +0700

    perf: implement infinite scroll

--- components/main-page/main-page.tsx
+++ components/main-page/main-page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 import { fetchPosts } from "@/actions/posts/get-posts";
 import ContentCard from "../content-card";
@@ -11,10 +13,31 @@ import { PostSkeleton } from "@/skeletons";
 import ErrorBanner from "../error-banner";
 
 export default function MainPage() {
-  const { data, error, isLoading } = useQuery({
+  const { ref, inView } = useInView();
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+    isLoading,
+  } = useInfiniteQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage =
+        lastPage.length === 20 ? allPages.length * 20 : undefined;
+      return nextPage;
+    },
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
 
   return (
     <div className="min-h-screen p-4">
@@ -26,14 +49,29 @@ export default function MainPage() {
         ) : error ? (
           <ErrorBanner />
         ) : (
-          data?.map((item: PostFiltered) => (
-            <div key={item.id}>
-              <ContentCard post={item} />
-              {data.length - 1 !== data.indexOf(item) && (
-                <Separator className="separator-color mx-auto my-4 w-2/3 bg-gray-700" />
-              )}
-            </div>
-          ))
+          data?.pages?.map((page) =>
+            page?.map((post: PostFiltered, index: number) => {
+              if (page.length == index + 1) {
+                return (
+                  <div key={post.id} ref={ref}>
+                    <ContentCard post={post} />
+                    {page.length - 1 !== page.indexOf(post) && (
+                      <Separator className="separator-color mx-auto my-4 w-2/3 bg-gray-700" />
+                    )}
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={post.id}>
+                    <ContentCard post={post} />
+                    {page.length - 1 !== page.indexOf(post) && (
+                      <Separator className="separator-color mx-auto my-4 w-2/3 bg-gray-700" />
+                    )}
+                  </div>
+                );
+              }
+            }),
+          )
         )}
       </div>
     </div>
